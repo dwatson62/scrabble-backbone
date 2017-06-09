280adf8ebea6ff4578504593122f79806af18feb
Daryl Watson <darylwtsn@gmail.com>
2017-06-09 10:54:44 +0100

    Setup jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,11 +19,16 @@ module.exports = function(grunt) {
         files: 'scss/*.scss',
         tasks: ['sass']
       }
+    },
+
+    jshint: {
+      all: ['app.js', 'Gruntfile.js', 'js/**/*.js', 'test/**/*.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('default', ['watch']);
 
