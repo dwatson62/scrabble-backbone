ef8c3c0a5e18b36a4024131fc511b20773ced714
Daryl Watson <darylwtsn@gmail.com>
2017-06-27 14:53:24 +0100

    Moved tile highlight functions to new helper object

--- js/helpers/tileHighlighter.js
+++ js/helpers/tileHighlighter.js
@@ -1,9 +1,6 @@
 var Scrabble = Scrabble || {};
 
 var tileHighlighter = {
-  placedLetters: this.placedLettersCollection,
-  tiles: this.boardTilesCollection,
-
   highlightAvailableTiles: function() {
     this.highlightAllPlacedAndConfirmedTiles();
     if (this._nothingPlayed()) {
@@ -15,6 +12,44 @@ var tileHighlighter = {
     } else {
       this.showNextAvailableTiles();
     }
+  },
+
+  highlightAllPlacedAndConfirmedTiles: function() {
+    this.boardTilesCollection.highlightAllPlacedTiles();
+    this.boardTilesCollection.highlightAllConfirmedTiles();
+  },
+
+  showAllNeighbourTiles: function() {
+    var self = this;
+    _.each(this.boardTilesCollection.allConfirmedTiles(), function(tile) {
+      var tileId = tile.get('tileId');
+      self.boardTilesCollection.showHorizontalAndVertical(tileId);
+    });
+  },
+
+  showHorizontalAndVertical: function() {
+    var firstTileId = this.placedLettersCollection.firstTileId();
+    this.boardTilesCollection.showHorizontalAndVertical(firstTileId);
+  },
+
+  showNextAvailableTiles: function() {
+    var firstTileId = this.placedLettersCollection.firstTileId();
+    var lastTileId = this.placedLettersCollection.lastTileId();
+    var direction = this.placedLettersCollection.determineDirection();
+
+    this.boardTilesCollection.showNextAvailableTiles(direction, firstTileId, lastTileId);
+  },
+
+  highlightCentreTile: function() {
+    this.boardTilesCollection.highlightCentreTile();
+  },
+
+  highlightAllTiles: function() {
+    this.boardTilesCollection.highlightAllTiles();
+  },
+
+  unhighlightAllTiles: function() {
+    this.boardTilesCollection.unhighlightAllTiles();
   },
 
   _nothingPlayed: function() {
--- js/views/boardView.js
+++ js/views/boardView.js
@@ -87,44 +87,5 @@ Scrabble.BoardView = Backbone.View
 
   currentPlayer: function() {
     return this.players.currentPlayer();
-  },
-
-  highlightAllPlacedAndConfirmedTiles: function() {
-    this.boardTilesCollection.highlightAllPlacedTiles();
-    this.boardTilesCollection.highlightAllConfirmedTiles();
-  },
-
-  showAllNeighbourTiles: function() {
-    var self = this;
-    _.each(this.boardTilesCollection.allConfirmedTiles(), function(tile) {
-      var tileId = tile.get('tileId');
-      self.boardTilesCollection.showHorizontalAndVertical(tileId);
-    });
-  },
-
-  showHorizontalAndVertical: function() {
-    var firstTileId = this.placedLettersCollection.firstTileId();
-    this.boardTilesCollection.showHorizontalAndVertical(firstTileId);
-  },
-
-  showNextAvailableTiles: function() {
-    var firstTileId = this.placedLettersCollection.firstTileId();
-    var lastTileId = this.placedLettersCollection.lastTileId();
-    var direction = this.placedLettersCollection.determineDirection();
-
-    this.boardTilesCollection.showNextAvailableTiles(direction, firstTileId, lastTileId);
-  },
-
-  highlightCentreTile: function() {
-    this.boardTilesCollection.highlightCentreTile();
-  },
-
-  highlightAllTiles: function() {
-    this.boardTilesCollection.highlightAllTiles();
-  },
-
-  unhighlightAllTiles: function() {
-    this.boardTilesCollection.unhighlightAllTiles();
-  },
-
+  }
 });
