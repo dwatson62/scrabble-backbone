33d1051bf8230162b4e6a1e39d2a58063c7819e2
Daryl Watson <darylwtsn@gmail.com>
2017-06-30 14:44:23 +0100

    tileHelper accepts tileNumber

--- js/helpers/tileHelper.js
+++ js/helpers/tileHelper.js
@@ -1,25 +1,28 @@
 var tileHelper = {
-  oneTileBelow: function(tileId) {
-    var splitId = tileId.split('_');
-    splitId[1] = String(parseInt(splitId[1]) + 1);
-    return splitId.join('_');
+  boundaryLeft: [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210],
+  boundaryRight: [14, 29, 44, 59, 74, 89, 104, 119, 134, 149, 164, 179, 194, 209, 224],
+
+  oneTileBelow: function(tileNumber) {
+    if (tileNumber <= 210) {
+      return tileNumber + 15;
+    }
   },
 
-  oneTileAbove: function(tileId) {
-    var splitId = tileId.split('_');
-    splitId[1] = String(parseInt(splitId[1]) - 1);
-    return splitId.join('_');
+  oneTileAbove: function(tileNumber) {
+    if (tileNumber >= 15) {
+      return tileNumber - 15;
+    }
   },
 
-  oneTileToLeft: function(tileId) {
-    var splitId = tileId.split('_');
-    splitId[2] = String(parseInt(splitId[2]) - 1);
-    return splitId.join('_');
+  oneTileToLeft: function(tileNumber) {
+    if (this.boundaryLeft.indexOf(tileNumber) === -1) {
+      return tileNumber - 1;
+    }
   },
 
-  oneTileToRight: function(tileId) {
-    var splitId = tileId.split('_');
-    splitId[2] = String(parseInt(splitId[2]) + 1);
-    return splitId.join('_');
+  oneTileToRight: function(tileNumber) {
+    if (this.boundaryRight.indexOf(tileNumber) === -1) {
+      return tileNumber + 1;
+    }
   }
 };
--- test/helpers/tileHelper.test.js
+++ test/helpers/tileHelper.test.js
@@ -1,28 +1,45 @@
-var Scrabble = Scrabble || {};
-var helper = tileHelper;
+var tileHelper = tileHelper || {};
 
 describe('Tile Helper', function() {
+  var helper = tileHelper;
+
   describe('#oneTileBelow', function() {
     it('returns the tile immediately below the given tileId', function() {
-      expect(helper.oneTileBelow('tile_7_7')).to.eql('tile_8_7');
+      expect(helper.oneTileBelow(112)).to.eql(127);
+    });
+
+    it('returns undefined if out of bounds', function() {
+      expect(helper.oneTileBelow(224)).to.be(undefined);
     });
   });
 
   describe('#oneTileAbove', function() {
     it('returns the tile immediately above the given tileId', function() {
-      expect(helper.oneTileAbove('tile_7_7')).to.eql('tile_6_7');
+      expect(helper.oneTileAbove(112)).to.eql(97);
+    });
+
+    it('returns undefined if out of bounds', function() {
+      expect(helper.oneTileAbove(0)).to.be(undefined);
     });
   });
 
   describe('#oneTileToLeft', function() {
     it('returns the tile immediately above the given tileId', function() {
-      expect(helper.oneTileToLeft('tile_7_7')).to.eql('tile_7_6');
+      expect(helper.oneTileToLeft(112)).to.eql(111);
+    });
+
+    it('returns undefined if out of bounds', function() {
+      expect(helper.oneTileToLeft(0)).to.be(undefined);
     });
   });
 
   describe('#oneTileToRight', function() {
     it('returns the tile immediately above the given tileId', function() {
-      expect(helper.oneTileToRight('tile_7_7')).to.eql('tile_7_8');
+      expect(helper.oneTileToRight(111)).to.eql(112);
+    });
+
+    it('returns undefined if out of bounds', function() {
+      expect(helper.oneTileToRight(14)).to.be(undefined);
     });
   });
 });
