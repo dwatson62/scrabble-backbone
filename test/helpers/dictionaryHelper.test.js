7165b4595d26ae892bdfc734fdfee8936f83f9b6
Daryl Watson <darylwtsn@gmail.com>
2017-06-30 17:33:24 +0100

    Tidied up dictionaryHelper.test

--- test/helpers/dictionaryHelper.test.js
+++ test/helpers/dictionaryHelper.test.js
@@ -15,48 +15,49 @@ describe('Dictionary Helper', function() {
     });
   });
 
+  var placeLetter = function(letter, tileNumber) {
+    var tile = boardTiles.fetchTile(tileNumber);
+    letter.place(tile);
+    tile.receiveLetter(letter);
+    placedLetters.add(letter);
+  };
+
+  var confirmLetter = function(letter) {
+    var tileNumber = letter.get('tileNumber');
+    var tile = boardTiles.fetchTile(tileNumber);
+    tile.confirm();
+    letter.confirm();
+  };
+
   describe('#prepareMainWord', function() {
     it('assembles just the placed letters if there are no surrounding letters', function() {
-      var c = Scrabble.LetterFactory.create({ value: 'a', tileNumber: 112 });
-      placeLetter(c);
-      var a = Scrabble.LetterFactory.create({ value: 'c', tileNumber: 111 });
-      placeLetter(a);
-      var t = Scrabble.LetterFactory.create({ value: 't', tileNumber: 113 });
-      placeLetter(t);
+      var c = Scrabble.LetterFactory.create({ value: 'a' });
+      placeLetter(c, 112);
+      var a = Scrabble.LetterFactory.create({ value: 'c' });
+      placeLetter(a, 111);
+      var t = Scrabble.LetterFactory.create({ value: 't' });
+      placeLetter(t, 113);
 
       expect(boardView.prepareMainWord()).to.eql('cat');
     });
 
     it('assembles placed letters with surrounding letters', function() {
-      var c = Scrabble.LetterFactory.create({ value: 'a', tileNumber: 112 });
-      placeLetter(c);
-      var a = Scrabble.LetterFactory.create({ value: 'c', tileNumber: 111 });
-      placeLetter(a);
-      var t = Scrabble.LetterFactory.create({ value: 't', tileNumber: 113 });
-      placeLetter(t);
+      var c = Scrabble.LetterFactory.create({ value: 'a' });
+      placeLetter(c, 112);
+      var a = Scrabble.LetterFactory.create({ value: 'c' });
+      placeLetter(a, 111);
+      var t = Scrabble.LetterFactory.create({ value: 't' });
+      placeLetter(t, 113);
 
-      var s = Scrabble.LetterFactory.create({ value: 's', tileNumber: 114 });
-      placeLetter(s);
+      var s = Scrabble.LetterFactory.create({ value: 's' });
+      placeLetter(s, 114);
       confirmLetter(s);
 
-      var o = Scrabble.LetterFactory.create({ value: 'o', tileNumber: 110 });
-      placeLetter(o);
+      var o = Scrabble.LetterFactory.create({ value: 'o' });
+      placeLetter(o, 110);
       confirmLetter(o);
 
       expect(boardView.prepareMainWord()).to.eql('ocats');
     });
-
-    var placeLetter = function(letter) {
-      var tile = boardTiles.fetchTile(letter.get('tileNumber'));
-      letter.place(tile);
-      tile.receiveLetter(letter);
-      placedLetters.add(letter);
-    };
-
-    var confirmLetter = function(letter) {
-      var tile = boardTiles.fetchTile(113);
-      tile.confirm();
-      letter.confirm();
-    };
   });
 });
