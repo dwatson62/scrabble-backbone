0c60a994eb7ab3a49cea60722e206b1322c5d09b
Daryl Watson <darylwtsn@gmail.com>
2017-07-27 11:27:34 +0100

    Encapsulated game setup

---
+++ js/game.js
@@ -0,0 +1,55 @@
+var Scrabble = Scrabble || {};
+
+var Game = function() {
+  this.bag = new Scrabble.LettersBag();
+  this.blankLetterView = new Scrabble.BlankLetterView();
+  this.boardTiles = new Scrabble.BoardTiles();
+  this.invalidWordView = new Scrabble.InvalidWordView();
+  this.placedLettersCollection = new Scrabble.PlacedLetters();
+  this.playedWordsView = new Scrabble.PlayedWordsView();
+};
+
+Game.prototype.create = function() {
+  var players = this._createPlayers();
+
+  var self = this;
+  players.each(function(player) {
+    self._createPlayerDashboard(player, this.indexOf(player));
+  }, players);
+
+  this._createBoard(players);
+
+  return this;
+};
+
+Game.prototype._assignLetters = function() {
+  var playerLetters = new Scrabble.PlayerLetters();
+  playerLetters.add(this.bag.retrieve(7));
+  return playerLetters;
+};
+
+Game.prototype._createBoard = function(players) {
+  return new Scrabble.BoardView({
+    boardTiles: this.boardTiles,
+    placedLettersCollection: this.placedLettersCollection,
+    players: players
+  });
+};
+
+Game.prototype._createPlayers = function() {
+  return new Scrabble.Players([
+    new Scrabble.Player({ name: 'Daryl' }),
+    new Scrabble.Player({ name: 'Brian' })
+  ]);
+};
+
+Game.prototype._createPlayerDashboard = function(player, index) {
+  var playerLetters = this._assignLetters();
+
+  return new Scrabble.PlayerDashboardView({
+    el: $('#player-dashboard-' + index),
+    bag: this.bag,
+    collection: playerLetters,
+    model: player
+  });
+};
--- js/scrabble.js
+++ js/scrabble.js
@@ -1,35 +1,10 @@
-var Scrabble = Scrabble || {};
+var Game = Game;
 
 _.templateSettings = {
   interpolate : /\{\{(.+?)\}\}/g
 };
 
 $(document).ready(function() {
-  var board = new Scrabble.BoardTiles();
-  var bag = new Scrabble.LettersBag();
-  var playedWordsView = new Scrabble.PlayedWordsView();
-  var blankLetterView = new Scrabble.BlankLetterView();
-  var invalidWordView = new Scrabble.InvalidWordView();
-
-  var players = new Scrabble.Players([
-    new Scrabble.Player({ name: 'Daryl' }),
-    new Scrabble.Player({ name: 'Brian' })
-  ]);
-
-  players.each(function(player) {
-    var playerLetters = new Scrabble.PlayerLetters();
-    playerLetters.add(bag.retrieve(7));
-    new Scrabble.PlayerDashboardView({
-      el: $('#player-dashboard-' + this.indexOf(player)),
-      bag: bag,
-      collection: playerLetters,
-      model: player
-    });
-  }, players);
-
-  var boardView = new Scrabble.BoardView({
-    boardTiles: board,
-    placedLettersCollection: new Scrabble.PlacedLetters(),
-    players: players
-  });
+  var game = new Game();
+  game.create();
 });
