9b9327ffe5d60ab29c34a09a6ccddd92836f5002
Daryl Watson <darylwtsn@gmail.com>
2017-07-28 15:59:25 +0100

    Setup require-js

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,12 +27,40 @@ module.exports = function(grunt) {
       options: {
         jshintrc: '.jshintrc'
       }
+    },
+
+    requirejs: {
+      dist: {
+        // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
+        options: {
+          almond: true,
+
+          replaceRequireScript: [{
+            files: ['dist/index.html'],
+            module: 'main'
+          }],
+
+          modules: [{name: 'main'}],
+          baseUrl: 'js/scripts',
+
+          mainConfigFile: 'js/scripts/main.js', // contains path specifications and nothing else important with respect to config
+
+          keepBuildDir: true,
+          dir: '.tmp/scripts',
+
+          optimize: 'none', // optimize by uglify task
+          useStrict: true,
+          wrap: true
+
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-requirejs');
 
   grunt.registerTask('default', ['watch']);
 
